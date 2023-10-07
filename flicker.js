5dfb84acdc4a9f51c51d22a644420cb46b8cdfe6
avtnmv <avtnmv@icloud.com>
2023-10-07 21:36:03 +0300

    feat: add typewriter v.2

--- flicker.js
+++ flicker.js
@@ -1,11 +1,11 @@
-const blinkingText = document.getElementById('flicker-text');
+// const blinkingText = document.getElementById('flicker-text');
 
-function toggleVisibility() {
-    if (blinkingText.style.visibility === 'visible') {
-      blinkingText.style.visibility = 'hidden';
-    } 
-    else {
-      blinkingText.style.visibility = 'visible';
-    }
-}
-setInterval(toggleVisibility, 500)
+// function toggleVisibility() {
+//     if (blinkingText.style.visibility === 'visible') {
+//       blinkingText.style.visibility = 'hidden';
+//     } 
+//     else {
+//       blinkingText.style.visibility = 'visible';
+//     }
+// }
+// setInterval(toggleVisibility, 500)
