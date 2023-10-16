49cc142baf7aeb896c521f647b843e45e47d24cb
avtnmv <avtnmv@icloud.com>
2023-10-16 20:00:23 +0300

    feat: add show more button

---
+++ loadMore.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("load-more");
+  const rect = document.getElementById("card-container");
+
+  button.addEventListener("click", () => {
+    toggleTwoClasses(rect, "is-visible", "is-hidden", 500);
+  });
+});
+
+function toggleTwoClasses(element, first, second, timeOfAnimation) {
+  if (!element.classList.contains(first)) {
+    element.classList.add(first);
+    element.classList.remove(second);
+  } else {
+    element.classList.add(second);
+    window.setTimeout(function() {
+      element.classList.remove(first);
+    }, timeOfAnimation);
+  }
+}
