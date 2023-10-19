d5b3d880bf0b212db8e8496b48f6645c5d21fe5e
avtnmv <avtnmv@icloud.com>
2023-10-19 20:20:52 +0300

    feat: add adaptive burger menu

--- btn.js
+++ btn.js
@@ -9,12 +9,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function toggleTwoClasses(element, first, second, timeOfAnimation) {
   if (!element.classList.contains(first)) {
-    element.classList.add(first);
-    element.classList.remove(second);
+  element.classList.add(first);
+  element.classList.remove(second);
   } else {
-    element.classList.add(second);
-    window.setTimeout(function() {
-      element.classList.remove(first);
-    }, timeOfAnimation);
+  element.classList.add(second);
+  window.setTimeout(function() {
+  element.classList.remove(first);
+  },timeOfAnimation);
   }
 }
+
+//Burger Menu
+let burgerMenu = document.querySelector('.burger-menu-unactive');
+let menu = document.querySelector('.navbar-burger-unactive');
+  
+burgerMenu.onclick = function() {
+  burgerMenu.classList.toggle('burger-menu-active');
+  menu.classList.toggle('navbar-burger-active');
+};
+
