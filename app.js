f1d0d19ab4f751a8efdc75f73aeb0aee4224044e
avtnmv <avtnmv@icloud.com>
2023-10-26 00:56:45 +0300

    feat: add smooth link anchor transition

--- app.js
+++ app.js
@@ -28,3 +28,29 @@ burgerMenu.onclick = function() {
   menu.classList.toggle('navbar-burger-active');
 };
 
+//Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      const duration = 700; // Продолжительность прокрутки в миллисекундах (например, 1000 миллисекунд = 1 секунда)
+      const targetOffset = targetElement.getBoundingClientRect().top;
+      const start = window.pageYOffset;
+      const startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
+
+      function scrollAnimation(currentTime) {
+        const elapsedTime = currentTime - startTime;
+        const progress = Math.min(elapsedTime / duration, 1);
+        window.scrollTo(0, start + targetOffset * progress);
+
+    if (progress < 1) {
+      requestAnimationFrame(scrollAnimation);
+    }
+  }
+    requestAnimationFrame(scrollAnimation);
+}
+});
+});
